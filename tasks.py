4b62f06b9e31783d246033f6b0e8057b7239cd95
Yulia3005 <petrashina-yulia@yandex.ru>
2020-07-06 22:53:53 +0300

    first-week

---
+++ tasks.py
@@ -0,0 +1,11 @@
+numbers = [2,8,107,6,24,-7,111]
+numbers.sort()
+print(numbers)
+numbers.sort(reverse = True)
+print(numbers)
+#
+c=list(set(a) & set(b))
+print(c)
+#
+def addition(a,b):
+	return(a+b)
